fd537dd156f37f406859056d2320127513db7b8a
Matthew Richardson <matt@dexterindustries.com>
2019-03-15 12:40:36 -0400

    Clean up line follower example

--- Python/Examples/LineFollower.py
+++ Python/Examples/LineFollower.py
@@ -14,24 +14,16 @@ from __future__ import division
 import time
 from di_sensors import line_follower
 
-print("Example program for reading a Dexter Industries Line Follower sensor on GPG3 AD1 port")
+print("Example program for reading a Dexter Industries Line Follower sensor on an I2C port")
 
-lf = line_follower.LineFollower(bus = "GPG3_AD1")
-
-'''
-cd ~/Dexter/DI_Sensors/Python
-sudo python setup.py install
-
-python ~/Dexter/DI_Sensors/Python/Examples/LineFollower.py
-
-'''
+lf = line_follower.LineFollower()
 
 print("Manufacturer     : %s" % lf.get_manufacturer())
 print("Name             : %s" % lf.get_board())
 print("Firmware Version : %d" % lf.get_version_firmware())
 
 while True:
-    # Read the line sensors values
+    # Read the line follower sensor values
     values = lf.read_sensors()
     str = ""
     for v in range(len(values)):
